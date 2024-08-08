0fc2a0ccaa75e8e54cf2173d043842beeadd68c3
Mohammad Reza <34370960+azibom@users.noreply.github.com>
2024-08-08 15:52:54 +0330

    Create Router.php

---
+++ src/Router.php
@@ -0,0 +1,29 @@
+<?php
+namespace ElegantGlacier;
+
+class Router {
+    protected $routes = []; // stores routes
+
+    public function addRoute(string $method, string $url, $target) {
+        $this->routes[$method][$url] = $target;
+    }
+
+    public function matchRoute() {
+        $method = $_SERVER['REQUEST_METHOD'];
+        $url = $_SERVER['REQUEST_URI'];
+        if (isset($this->routes[$method])) {
+            foreach ($this->routes[$method] as $routeUrl => $target) {
+                // Use named subpatterns in the regular expression pattern to capture each parameter value separately
+                $pattern = preg_replace('/\/:([^\/]+)/', '/(?P<$1>[^/]+)', $routeUrl);
+                if (preg_match('#^' . $pattern . '$#', $url, $matches)) {
+                    // Pass the captured parameter values as named arguments to the target function
+                    $params = array_filter($matches, 'is_string', ARRAY_FILTER_USE_KEY); // Only keep named subpattern matches
+                    call_user_func_array($target, $params);
+                    return;
+                }
+            }
+        }
+
+//        throw new \Exception('Route not found');
+    }
+}
